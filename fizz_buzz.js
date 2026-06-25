de786393d777b0894cf7df821c935b57475a9ab6
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-06-24 23:16:13 -0600

    I changed it to the for loop

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,17 +1,16 @@
 let output = [];
-let x = 1;
+
 
 function fizzbuzz() {    
-   while (x <= 100){
-      x++;
-      if(x % 3 === 0 && x % 5 === 0){
+  for (let i = 1; i <= 100; i++){      
+      if(i % 3 === 0 && i % 5 === 0){
          output.push('FizzBuzz')
-      }else if(x %5 == 0){
+      }else if(i %5 == 0){
          output.push("Buzz"); 
-      }else if(x % 3 == 0){
+      }else if(i % 3 == 0){
          output.push("Fizz")
       }else {
-         output.push(x);
+         output.push(i);
       }
    }
 
