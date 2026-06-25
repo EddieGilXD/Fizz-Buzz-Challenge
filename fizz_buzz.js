b4d9efba47694783f8ff0c5cfe8a129d39e71a14
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-06-24 22:44:22 -0600

    I added a while loop to count until 100

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,40 +1,22 @@
 let output = [];
-let x = 0;
+let x = 1;
 
 function fizzbuzz() {    
-   x++;
-   if(x % 3 === 0 && x % 5 === 0){
-      output.push('FizzBuzz')
-   }else if(x %5 == 0){
-      output.push("Buzz"); 
-   }else if(x % 3 == 0){
-      output.push("Fizz")
-   }else {
-      output.push(x);
+   while (x <= 100){
+      x++;
+      if(x % 3 === 0 && x % 5 === 0){
+         output.push('FizzBuzz')
+      }else if(x %5 == 0){
+         output.push("Buzz"); 
+      }else if(x % 3 == 0){
+         output.push("Fizz")
+      }else {
+         output.push(x);
+      }
    }
-   
 
    console.log(output)
 }
 
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
-fizzbuzz();
 fizzbuzz();
 
