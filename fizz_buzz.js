81c0eba0b3e04fcf58856b8d3d59c83fa1f02331
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-06-24 21:28:40 -0600

    Fizz Buzz completed

---
+++ fizz_buzz.js
@@ -0,0 +1,25 @@
+let output = [];
+let x = 0;
+
+function fizzbuzz() {    
+   x++;
+   if(x % 3 === 0 && x % 5 === 0){
+      output.push('FizzBuzz')
+   }else if(x %5 == 0){
+      output.push("Buzz"); 
+   }else if(x % 3 == 0){
+      output.push("Fizz")
+   }else {
+      output.push(x);
+   }
+   
+
+   console.log(output)
+}
+
+fizzbuzz();
+fizzbuzz();
+fizzbuzz();
+fizzbuzz();
+fizzbuzz();
+
